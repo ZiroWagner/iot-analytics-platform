320943d4ad515ef68da73614da49b8ac504c08dd
unknown <ZiroWagner>
2026-05-14 17:12:51 -0500

    Close backend test resources to prevent Jest hanging

--- backend/src/prisma/prisma.service.spec.ts
+++ backend/src/prisma/prisma.service.spec.ts
@@ -4,9 +4,10 @@ import { PrismaService } from './prisma.service';
 
 describe('PrismaService', () => {
   let service: PrismaService;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
       providers: [
         PrismaService,
         {
@@ -19,6 +20,10 @@ describe('PrismaService', () => {
     }).compile();
 
     service = module.get<PrismaService>(PrismaService);
+  });
+
+  afterEach(async () => {
+    await module.close();
   });
 
   it('should be defined', () => {
